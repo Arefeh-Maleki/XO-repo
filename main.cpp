e2893892d3baa64c6106981dda364fe9d0f441b6
Arefeh <arefeh.mlk@gmail.com>
2022-07-11 22:14:27 +0430

    second edition of XO game

--- main.cpp
+++ main.cpp
@@ -1,192 +1,101 @@
 #include <iostream>
 #include <vector>
 #include <conio.h>
-
 using namespace std;
 
-void makeTable(vector<vector<char>> &table);
+int pointerRow = 1, pointerCol = 1;
+char turn = 'X';
+vector<vector<char>> table = {{'-', '-', '-'},
+                              {'-', '-', '-'},
+                              {'-', '-', '-'}};
 
-void printTable(vector<vector<char>> &table);
-
-void clear();
-
-void userOrder(vector<vector<char>> &table);
-
-int win(vector<vector<char>> table);
-
-void up(vector<vector<char>> table);
-
-void down(vector<vector<char>> table);
-
-void left(vector<vector<char>> table);
-
-void right(vector<vector<char>> table);
-
-void chooseXO(vector<vector<char>> &table);
-
-int i = 1, j = 1, counter = 1;
-
-int main() {
-    vector<vector<char>> table;
-    makeTable(table);
-    table[1][1] = '*';
-    printTable(table);
-    table[1][1] = '-';
-    while (!win(table)) {
-        userOrder(table);
-    }
-    if (win(table)) {
-        cout << "X won!";
-    } else if (win(table) == 2)
-        cout << "O won!";
+void clear() {
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
-void makeTable(vector<vector<char>> &table) {
-    const int size = 3;
-    for (int n = 0; n < size; n++) {
-        vector<char> dashT;
-        for (int m = 0; m < size; m++) {
-            dashT.push_back('-');
-        }
-        table.push_back(dashT);
-    }
-}
-
-void printTable(vector<vector<char>> &table) {
+void printTable() {
     clear();
     for (int row = 0; row < table.size(); row++) {
         for (int col = 0; col < table[row].size(); col++) {
-            cout << table[row][col] << "  ";
+            if (row == pointerRow && col == pointerCol && table[row][col] == '-') {
+                cout << "*" << "  ";
+            }
+            else
+                cout << table[row][col] << "  ";
         }
         cout << "\b\n";
     }
 }
 
-void userOrder(vector<vector<char>> &table) {
+void up() {
+    pointerRow = max (pointerRow - 1, 0);
+}
+
+void down() {
+    pointerRow = min (pointerRow + 1, 2);
+}
+
+void left() {
+    pointerCol = max (pointerCol - 1, 0);
+}
+
+void right() {
+    pointerCol = min (pointerCol + 1, 2);
+}
+
+void chooseXO() {
+    if (table[pointerRow][pointerCol] == '-') {
+        table[pointerRow][pointerCol] = turn;
+        turn = (turn == 'X') ? 'O' : 'X';
+    }
+}
+
+void userOrder() {
     char direction;
     direction = getch();
     direction = (char) tolower(direction);
     switch (direction) {
         case 'w' :
-            up(table);
+            up();
             break;
         case 's' :
-            down(table);
+            down();
             break;
         case 'a' :
-            left(table);
+            left();
             break;
         case 'd' :
-            right(table);
+            right();
             break;
         case 'p' :
-            chooseXO(table);
+            chooseXO();
             break;
         case 'q' :
             exit(0);
     }
+    printTable();
 }
 
-void up(vector<vector<char>> table) {
-    if (i != 0) {
-        if (table[i - 1][j] == '-')
-            table[--i][j] = '*';
-        else
-            --i;
-        printTable(table);
-    }
+int checkWin() {
+    if ( (table[0][0] != '-' && table[0][0] == table[0][1] && table[0][1] == table[0][2]) ||
+         (table[1][0] != '-' && table[1][0] == table[1][1] && table[1][1] == table[1][2]) ||
+         (table[2][0] != '-' && table[2][0] == table[2][1] && table[2][1] == table[2][2]) ||
+         (table[0][0] != '-' && table[0][0] == table[1][0] && table[1][0] == table[2][0]) ||
+         (table[0][1] != '-' && table[0][1] == table[1][1] && table[1][1] == table[2][1]) ||
+         (table[0][2] != '-' && table[0][2] == table[1][2] && table[1][2] == table[2][2]) ||
+         (table[0][0] != '-' && table[0][0] == table[1][1] && table[1][1] == table[2][2]) ||
+         (table[0][2] != '-' && table[0][2] == table[1][1] && table[1][1] == table[2][0]) )
+        return true;
+    return false;
 }
 
-void down(vector<vector<char>> table) {
-    if (i != 2) {
-        if (table[i + 1][j] == '-')
-            table[++i][j] = '*';
-        else
-            ++i;
-        printTable(table);
+int main() {
+    printTable();
+    while (!checkWin()) {
+        userOrder();
     }
-}
-
-void left(vector<vector<char>> table) {
-    if (j != 0) {
-        if (table[i][j - 1] == '-')
-            table[i][--j] = '*';
-        else
-            --j;
-        printTable(table);
-    }
-}
-
-void right(vector<vector<char>> table) {
-    if (j != 2) {
-        if (table[i][j + 1] == '-')
-            table[i][++j] = '*';
-        else
-            ++j;
-        printTable(table);
-    }
-}
-
-void chooseXO(vector<vector<char>> &table) {
-    if (table[i][j] == '-') {
-        if (counter % 2 == 1) {
-            table[i][j] = 'X';
-            counter++;
-        } else if (counter % 2 == 0) {
-            table[i][j] = 'O';
-            counter++;
-        }
-    }
-    printTable(table);
-}
-
-int win(vector<vector<char>> table) {
-    int flag = 0;
-    if (table[0][0] == table[1][1] && table[0][0] == table[2][2]) {
-        if (table[0][0] == 'X') {
-            flag = 1;
-            return flag;
-        } else if (table[0][0] == 'O') {
-            flag = 2;
-            return flag;
-        }
-    }
-    if (table[0][2] == table[1][1] && table[0][2] == table[2][0]) {
-        if (table[0][2] == 'X') {
-            flag = 1;
-            return flag;
-        } else if (table[0][2] == 'O') {
-            flag = 2;
-            return flag;
-        }
-    }
-    for (int row = 0; row < table.size(); row++) {
-        int col = 0;
-        if (table[row][col] == table[row][++col] && table[row][col] == table[row][++col]) {
-            if (table[row][col] == 'X') {
-                flag = 1;
-                return flag;
-            } else if (table[row][col] == 'O') {
-                flag = 2;
-                return flag;
-            }
-        }
-    }
-    for (int col = 0; col < table.size(); col++) {
-        int row = 0;
-        if (table[row][col] == table[++row][col] && table[row][col] == table[++row][col]) {
-            if (table[row][col] == 'X') {
-                flag = 1;
-                return flag;
-            } else if (table[row][col] == 'O') {
-                flag = 2;
-                return flag;
-            }
-        }
-    }
-    return flag;
-}
-
-void clear() {
-    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    if (turn == 'O') {
+        cout << "X won!";
+    } else if (turn == 'X')
+        cout << "O won!";
 }
